8affd283eb1d8c71fe15046a80f169b14d665429
Borja Montseny <68342939+borjaMontseny@users.noreply.github.com>
2023-09-21 18:52:59 +0200

    Commit

--- Apartat 2/exercici.js
+++ Apartat 2/exercici.js
@@ -2,7 +2,7 @@
 do {
     let frase = prompt("Introdueix una frase, o 0 per acabar");
     if (frase !== "0") {
-        let fraseMayus = frase.toLocaleUpperCase();
+        let fraseMayus = frase.toUpperCase;
         document.write("<p>Frase: <b>" + frase + "</b></p>");
         document.write("<p>A: " + fraseMayus.replace(/[^A]/g, "").length + "</p>");
         document.write("<p>E: " + fraseMayus.replace(/[^E]/g, "").length + "</p>");
